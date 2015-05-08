700b11cb373f239f94dcda4c17e6b1b9a52edae9
Visvk <viktor.sincak@gmail.com>
2015-05-08 16:15:28 +0200

    TaskDefault: mailto solver

--- app/model/Proj_usRepository.php
+++ app/model/Proj_usRepository.php
@@ -48,7 +48,7 @@ class Proj_usRepository extends Repository
 	public function getUsersAndManagerByProjectId($projectId)
 	{
 		return $this->getTable()
-			->select('user_subj.user_id, user_subj.user.login, manager, user_subj.user.surname, user_subj.user.name')
+			->select('user_subj.user_id, user_subj.user.login, manager, user_subj.user.surname, user_subj.user.name, user_subj.user.email')
 			->where('project_id',$projectId);
 	}
 
